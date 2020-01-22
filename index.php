a338f5af8c263c8f30084ece284e51866d5968b3
Ricardo Medina <rcrdmedina6@gmail.com>
2020-01-22 09:24:23 -0400

    creando manifest.jso

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Template UltraCreditos.com</title>
+  <link href="styles.css" rel="stylesheet">
+
+  <link rel="manifest" href="./manifest.json">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="application-name" content="UltraCreditos.">
+  <meta name="apple-mobile-web-app-title" content="UltraCreditos.">
+  <meta name="theme-color" content="#166ae7">
+  <meta name="msapplication-navbutton-color" content="#166ae7">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <meta name="msapplication-starturl" content="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/png" sizes="512x512" href="./assets/favicon/android-chrome-512x512.png">
+  <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="./assets/favicon/android-chrome-512x512.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="./assets/favicon/android-chrome-192x192.png">
+  <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="./assets/favicon/android-chrome-192x192.png">
+  <link rel="icon" type="image/png" sizes="150x150" href="./assets/favicon/mstile-150x150.png">
+  <link rel="apple-touch-icon" type="image/png" sizes="150x150" href="./assets/favicon/mstile-150x150.png">
+  <link rel="icon" type="image/png" sizes="144x144" href="./assets/favicon/mstile-144x144.png">
+  <link rel="apple-touch-icon" type="image/png" sizes="144x144" href="./assets/favicon/mstile-144x144.png">
+  <link rel="icon" type="image/png" sizes="310x150" href="./assets/favicon/mstile-310x150.png">
+  <link rel="apple-touch-icon" type="image/png" sizes="310x150" href="./assets/favicon/mstile-310x150.png">
+  <link rel="icon" type="image/png" sizes="310x310" href="./assets/favicon/mstile-310x310.png">
+  <link rel="apple-touch-icon" type="image/png" sizes="310x310" href="./assets/favicon/mstile-310x310.png">
+  <link rel="icon" type="image/png" sizes="70x70" href="./assets/favicon/mstile-70x70.png">
+  <link rel="apple-touch-icon" type="image/png" sizes="70x70" href="./assets/favicon/mstile-70x70.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon/favicon-32x32.png">
+  <link rel="apple-touch-icon" type="image/png" sizes="32x32" href="./assets/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon/favicon-16x16.png">
+  <link rel="apple-touch-icon" type="image/png" sizes="16x16" href="./assets/favicon/favicon-16x16.png">
+
+</head>
+<body>
+  <div class="container mx-auto flex flex-col min-h-screen">
+    <header class="ucr-header fixed z-30 top-0 inset-x-0 h-12 md:h-16 lg:h-16 xl:h-16 flex items-center justify-center">
+      <div class="md:w-11/12 lg:w-11/12 xl:w-9/12 flex items-center justify-center h-full">
+        <div class="flex items-center justify-center flex-1 h-full">
+          <a class="w-full h-full flex items-center justify-center cursor-pointer no-underline outline-none" href="#">
+            <img title="UltraCreditos.com" alt="UltraCreditos.com" class="w-40 md:w-32 lg:w-40 xl:w-40 relative logo"
+              src="./assets/images/logo-uc-blanco.png" />
+          </a>
+        </div>
+        <div class="flex-2 h-full mobile-hidden">
+          <nav class="flex items-center justify-center h-full w-full">
+            <ul class="flex items-center justify-center h-full w-full">
+              <li class="flex flex-1 items-center justify-center h-full">
+                <a href="quienes-somos.php" class="flex flex-1 items-center justify-center h-full text-xs lg:text-sm xl:text-sm text-center text-white cursor-pointer no-underline outline-none">
+                  Quiénes somos
+                </a>
+              </li>
+              <li class="flex flex-1 items-center justify-center h-full">
+                <a href="#" class="flex flex-1 items-center justify-center h-full text-xs lg:text-sm xl:text-sm text-center text-white cursor-pointer no-underline outline-none">
+                  Cómo funciona
+                </a>
+              </li>
+              <li class="flex flex-1 items-center justify-center h-full text-center">
+                <a href="#" class="flex flex-1 items-center justify-center h-full text-xs lg:text-sm xl:text-sm text-center text-white cursor-pointer no-underline outline-none">
+                  Bancos Participantes
+                </a>
+              </li>
+              <li class="flex flex-1 items-center justify-center h-full">
+                <a href="#" class="flex flex-1 items-center justify-center h-full text-xs lg:text-sm xl:text-sm text-center text-white cursor-pointer no-underline outline-none">
+                  Contáctanos
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="flex flex-1 h-full items-center justify-center mobile-hidden">
+          <button class="btn text-center text-white cursor-pointer border-solid border-2 border-white px-5 rounded-full text-xs lg:text-sm xl:text-sm py-1">
+            INGRESAR
+          </button>
+        </div>
+      </div>
+    </header>
+    <section class="ucr-main flex flex-1 z-10 items-end">
+      <div class="bg-page">
+        <img class="w-full h-full mobile-hidden" src="./assets/images/bg-ucr.png" srcset="./assets/images/bg-ucr.png 1x,./assets/images/bg-ucr.png  2x, ./assets/images/bg-ucr.png  3x" />
+        <img class="w-full h-full mobile-show hidden" src="./assets/images/bg-ucr-mobile.png" />
+      </div>
+      <div class="w-10/12 md:w-2/3 lg:w-2/3 xl:w-2/3 mx-auto">
+        <h1 class="text-white text-center md:text-2xl lg:text-3xl xl:text-3xl font-bold mobile-hidden mb-2">¿Qué clase
+          de crédito estás buscando?</h1>
+        <div class="flex items-center justify-center">
+          <a href="#" class="link-icon w-2/6 md:w-1/4 lg:w-1/4 xl:w-1/4 m-2 rounded-lg bg-white h-24 custom-lg-h-26 xl:h-32 shadow no-underline cursor-pointer">
+            <div class="content-icon relative rounded-lg w-full flex flex-col items-center justify-center">
+              <img class="w-12 lg:w-12 xl:w-16 current" alt="Crédito de Vivienda" title="Crédito de Vivienda" src="./assets/images/icon-credito-vivienda.svg" />
+              <img class="w-12 lg:w-12 xl:w-16 hover" alt="Crédito de Vivienda" title="Crédito de Vivienda" src="./assets/images/icon-credito-vivienda-hover.svg" />
+            </div>
+            <div class="content-icon-text flex items-start justify-center mt-1">
+              <strong class="flex items-start justify-center text-xs custom-md:text-xs lg:text-xs xl:text-sm">Crédito
+                de Vivienda</strong>
+            </div>
+          </a>
+          <a href="#" class="link-icon w-2/6 md:w-1/4 lg:w-1/4 xl:w-1/4 m-2 rounded-lg bg-white h-24 custom-lg-h-26 xl:h-32 shadow no-underline cursor-pointer">
+            <div class="content-icon relative rounded-lg w-full flex flex-col items-center justify-center">
+              <img class="w-12 lg:w-12 xl:w-16 current" alt="Crédito de Consumo" title="Crédito de Consumo" src="./assets/images/icon-credito-consumo.svg" />
+              <img class="w-12 lg:w-12 xl:w-16 hover" alt="Crédito de Consumo" title="Crédito de Consumo" src="./assets/images/icon-credito-consumo-hover.svg" />
+            </div>
+            <div class="content-icon-text flex items-start justify-center mt-1">
+              <strong class="flex items-start justify-center text-xs custom-md:text-xs lg:text-xs xl:text-sm">Crédito
+                de Consumo</strong>
+            </div>
+          </a>
+          <a href="#" class="link-icon w-2/6 md:w-1/4 lg:w-1/4 xl:w-1/4 m-2 rounded-lg bg-white h-24 custom-lg-h-26 xl:h-32 shadow no-underline cursor-pointer">
+            <div class="content-icon relative rounded-lg w-full flex flex-col items-center justify-center">
+              <img class="w-12 lg:w-12 xl:w-16 current" alt="Abre tu Cuenta" title="Abre tu Cuenta" src="./assets/images/icon-abrir-cuenta.png" />
+              <img class="w-12 lg:w-12 xl:w-16 hover" alt="Abre tu Cuenta" title="Abre tu Cuenta" src="./assets/images/icon-abrir-cuenta.png" />
+            </div>
+            <div class="content-icon-text flex items-start justify-center mt-1">
+              <strong class="flex items-start justify-center text-xs custom-md:text-xs lg:text-xs xl:text-sm">Abre tu
+                Cuenta</strong>
+            </div>
+          </a>
+        </div>
+        <div class="flex items-center justify-center">
+          <a href="#" class="link-icon w-2/6 md:w-1/4 lg:w-1/4 xl:w-1/4 m-2 rounded-lg bg-white h-24 custom-lg-h-26 xl:h-32 shadow no-underline cursor-pointer">
+            <div class="content-icon relative rounded-lg w-full flex flex-col items-center justify-center">
+              <img class="w-12 lg:w-12 xl:w-16 current" alt="Tarjeta de Crédito" title="Tarjeta de Crédito" src="./assets/images/icon-credito-tarjeta.svg" />
+              <img alt="Tarjeta de Crédito" title="Tarjeta de Crédito" class="w-12 lg:w-12 xl:w-16 hover" src="./assets/images/icon-credito-tarjeta.svg" />
+            </div>
+            <div class="content-icon-text flex items-start justify-center mt-1">
+              <strong class="flex items-start justify-center text-xs custom-md:text-xs lg:text-xs xl:text-sm">Tarjeta
+                de Crédito</strong>
+            </div>
+          </a>
+          <a href="#" class="link-icon w-2/6 md:w-1/4 lg:w-1/4 xl:w-1/4 m-2 rounded-lg bg-white h-24 custom-lg-h-26 xl:h-32 shadow no-underline cursor-pointer">
+            <div class="content-icon relative rounded-lg w-full flex flex-col items-center justify-center">
+              <img class="w-12 lg:w-12 xl:w-16 current" alt="Crédito vehícular" title="Crédito vehícular" src="./assets/images/icon-credito-vehicular.svg" />
+              <img class="w-12 lg:w-12 xl:w-16 hover" alt="Crédito vehícular" title="Crédito vehícular" src="./assets/images/icon-credito-vehicular-hover.svg" />
+            </div>
+            <div class="content-icon-text flex items-start justify-center mt-1">
+              <strong class="flex items-start justify-center text-xs custom-md:text-xs lg:text-xs xl:text-sm">Crédito
+                vehícular</strong>
+            </div>
+          </a>
+          <a href="#" class="link-icon w-2/6 md:w-1/4 lg:w-1/4 xl:w-1/4 m-2 rounded-lg bg-white h-24 custom-lg-h-26 xl:h-32 shadow no-underline cursor-pointer">
+            <div class="content-icon relative rounded-lg w-full flex flex-col items-center justify-center">
+              <img class="w-12 lg:w-12 xl:w-16 current" alt="Crédito MYPE" title="Crédito MYPE" src="./assets/images/icon-credito-mype.svg" />
+              <img class="w-12 lg:w-12 xl:w-16 hover" alt="Crédito MYPE" title="Crédito MYPE" src="./assets/images/icon-credito-mype-hover.svg" />
+            </div>
+            <div class="content-icon-text flex items-start justify-center mt-1">
+              <strong class="flex items-start justify-center text-xs custom-md:text-xs lg:text-xs xl:text-sm">Crédito
+                MYPE</strong>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+    <footer class="ucr-footer flex flex-col items-center justify-center md:justify-end lg:justify-end xl:justify-end">
+      <button id="btnInformation" class="btn a-link mobile-show hidden items-center justify-center text-2xl outline-none cursor-pointer">Más
+        información</button>
+      <p class="paragraph mobile-hidden text-center text-xs xl:text-sm leading-normal my-1 w-11/12 lg:w-10/12 xl:w-10/12">
+        UltraCreditos.com es un Comparador de Créditos que canaliza clientes a los Bancos participantes.
+        Las cuotas y montos de financiamiento son aproximados y cada Banco determina la cuota final al concluir el
+        proceso de evaluación crediticia.
+        Los bancos participantes de UltraCreditos.com son entidades financieras supervisadas por ASFI.
+        UltraCreditos.com no es una entidad financiera y no otorga créditos.
+      </p>
+      <p class="paragraph mobile-hidden text-center text-xs xl:text-sm leading-normal my-1 w-11/12 lg:w-10/12 xl:w-10/12">
+        <span>Al hacer uso de este sitio aceptas los</span>
+        <a href="#" class="a-link cursor-pointer no-underline">Términos y Condiciones</a>
+        <span>de UltraCreditos.com</span>
+      </p>
+      <p class="copyright mobile-hidden text-center text-xs xl:text-sm leading-normal">
+        © 2019 UltraCreditos.com Todos los derechos reservados.
+      </p>
+    </footer>
+  </div>
+  <div id="modal" class="modal flex flex-col w-full h-full fixed z-30 inset-0 items-center justify-center">
+    <div class="modal-content relative bg-white w-4/5 rounded-lg py-8 flex flex-col items-center justify-center shadow">
+      <div class="modal-content-close">
+        <span id="modalBtnClose" class="modal-btn-close absolute outline-none cursor-pointer">X</span>
+      </div>
+      <div class="modal-header flex items-center justify-center">
+        <img title="UltraCreditos.com" alt="UltraCreditos.com" class="w-40" src="./assets/images/logo-uc.png" />
+      </div>
+      <div class="modal-body flex flex-col items-center justify-center mt-4">
+        <p class="text-center text-xs leading-normal my-1 w-4/5">
+          UltraCreditos.com es un Comparador de Créditos en Bolivia que canaliza clientes a los Bancos participantes.
+        </p>
+        <p class="text-center text-xs leading-normal my-1 w-4/5">
+          Las cuotas y montos de financiamiento son aproximados y cada Banco determina la cuota final al concluir el
+          proceso de evaluación crediticia. Los bancos participantes de UltraCreditos.com son entidades financieras
+          supervisadas por ASFI. UltraCreditos.com no es una entidad financiera y no otorga créditos.
+        </p>
+        <p class="text-center text-xs leading-normal w-4/5">
+          <span>Al hacer uso de este sitio aceptas los</span>
+          <a class="a-link cursor-pointer no-underline" href="#">Términos y Condiciones</a>
+          <span>de UltraCreditos.com</span>
+        </p>
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+<script type="text/javascript" src="main.js"></script></body>
+
+</html>
